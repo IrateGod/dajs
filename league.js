8190128a65872de636b007014c9a1013b0709470
IrateGod <konamijudge@yahoo.com>
2015-03-23 13:52:39 +0100

    Update league.js

--- league.js
+++ league.js
@@ -1,21 +1,80 @@
-var rankMap = {
+function Map(o) {
+    var p;
+    if (!(this instanceof Map)) {
+        return new Map(o);
+    }
+    for (p in o) {
+        if (o.hasOwnProperty(p) && o.propertyIsEnumerable(p) && !(p in this)) {
+            this[p] = o[p];
+        }
+    }
+    return this;
+}
+Map.prototype.map = function(s) {
+    return this[s] || this[s.toLowerCase()] || this[s.toUpperCase()] || null;
+};
+var rankMap = Map({
         0: {
             rank: "Slifer Red",
-            elo: 500
+            elo: 500,
+            rankID: 0
         },
         1: {
             rank: "Ra Yellow",
-            elo: 750
+            elo: 750,
+            rankID: 1
         },
         2: {
             rank: "Obelisk Blue",
-            elo: 1000
+            elo: 1000,
+            rankID: 2
         },
         3: {
             rank: "Society of Light",
-            elo: 1250
+            elo: 1250,
+            rankID: 3
+        },
+        "slifer": {
+            rank: "Slifer Red",
+            elo: 500,
+            rankID: 0
+        },
+        "ra": {
+            rank: "Ra Yellow",
+            elo: 750,
+            rankID: 1
+        },
+        "obelisk": {
+            rank: "Obelisk Blue",
+            elo: 1000,
+            rankID: 2
+        },
+        "sol": {
+            rank: "Society of Light",
+            elo: 1250,
+            rankID: 3
+        },
+        "slifer red": {
+            rank: "Slifer Red",
+            elo: 500,
+            rankID: 0
+        },
+        "ra yellow": {
+            rank: "Ra Yellow",
+            elo: 750,
+            rankID: 1
+        },
+        "obelisk blue": {
+            rank: "Obelisk Blue",
+            elo: 1000,
+            rankID: 2
+        },
+        "society of light": {
+            rank: "Society of Light",
+            elo: 1250,
+            rankID: 3
         }
-    },
+    }),
     dbTopic = "/t41235-",
     entries = {},
     matches = [],
@@ -36,23 +95,36 @@ function calculateNewElo(opts) {
         pointsLost,
         newWinnerElo,
         newLoserElo,
-        rankDifference = entries[opts.winner].rank - entries[opts.loser].rank,
+        rankDifference = entries[opts.winner].rankID - entries[opts.loser].rankID,
         winnerWLRatio = (entries[opts.winner].wins / (entries[opts.winner].wins + entries[opts.winner].losses)) * 100,
         loserWLRatio = (entries[opts.loser].wins / (entries[opts.loser].wins + entries[opts.loser].losses)) * 100,
         winnerWLString = winnerWLRatio.toFixed(1).replace(/\.0$/, '') + '%',
         loserWLString = loserWLRatio.toFixed(1).replace(/\.0$/, '') + '%',
         hardCapGainPercentage = 60,
-        hardCapLossPercentage = 40,
-        ifClauseDebugVar;
+        hardCapLossPercentage = 40;
+    entries[opts.winner].winLossHistory.push(1);
+    entries[opts.loser].winLossHistory.push(0);
     if (rankDifference > 0) { // higher rank won vs lower rank 
         pointsGained = Math.floor(staticPointsGained / (rankDifference + (rankDifference * 0.1)));
         pointsLost = (rankDifference >= 2) ? Math.floor(pointsGained / rankDifference) : Math.ceil(pointsGained / 2);
+        entries[opts.winner].lowerPlayed++;
+        entries[opts.winner].lowerWon++;
+        entries[opts.loser].higherPlayed++;
+        entries[opts.loser].higherLost++;
     } else if (rankDifference < 0) { // lower rank won vs higher rank
         pointsGained = (rankDifference <= -2) ? Math.round(staticPointsGained * Math.abs(rankDifference)) : Math.round(staticPointsGained * 1.5);
         pointsLost = pointsGained;
+        entries[opts.winner].higherPlayed++;
+        entries[opts.winner].higherWon++;
+        entries[opts.loser].lowerPlayed++;
+        entries[opts.loser].lowerLost++;
     } else if (rankDifference === 0) { // same rank
         pointsGained = staticPointsGained;
         pointsLost = staticPointsLost;
+        entries[opts.winner].equalPlayed++;
+        entries[opts.winner].equalWon++;
+        entries[opts.loser].equalPlayed++;
+        entries[opts.loser].equalLost++;
     }
     if (Math.round(winnerWLRatio) > hardCapGainPercentage) {
         pointsGained = pointsGained + 10;
@@ -63,7 +135,7 @@ function calculateNewElo(opts) {
     } else if (Math.round(winnerWLRatio) <= 50) {
         pointsGained = pointsGained - (Math.round(50 - winnerWLRatio));
     }
-	pointsGained = (pointsGained <= 0) ? 1 : pointsGained;
+    pointsGained = (pointsGained <= 0) ? 1 : pointsGained;
     if (Math.round(loserWLRatio) > hardCapGainPercentage) {
         pointsLost = pointsLost - 10;
     } else if (Math.round(loserWLRatio) < hardCapLossPercentage) {
@@ -80,19 +152,54 @@ function calculateNewElo(opts) {
     entries[opts.loser].elo = newLoserElo;
     entries[opts.loser].winratio = loserWLString;
 }
+
+function makeTable(sourceArray) {
+    var returnHTML = "";
+    sourceArray.forEach(function(v, i) {
+        returnHTML = returnHTML + '<tr class="rankingTable rank_' + (i + 1) + '"><td class="rankingTableUser">' + v.username + '</td><td class="rankingTableWins">' + v.wins + '</td><td class="rankingTableLosses">' + v.losses + '</td><td class="rankingTableElo">' + v.elo + '</td></tr>';
+    });
+    return returnHTML;
+}
+
+function sortAfter(sourceArray, sortString, ascDesc) {
+    return sourceArray.sort(function(a, b) {
+        if (sortString === "username") {
+            return (ascDesc ? ((b.username[0].toLowerCase() < a.username[0].toLowerCase() ? 1 : -1)) : ((b.username[0].toLowerCase() < a.username[0].toLowerCase()) ? -1 : 1));
+        } else {
+            return (ascDesc ? b[sortString] - a[sortString] : a[sortString] - b[sortString]);
+        }
+    });
+}
+
 $(function() {
     $.get(dbTopic, function(data, status, xhr) {
         if (xhr.status !== 200) {
-            console.log('HTTP Request failed with Error Status of ' + xhr.status + ', please check your connection settings or connect your system administrator.');
+            console.log('Error "' + status + '"\nHTTP Request failed with Error Status of ' + xhr.status + ', please check your connection settings or connect your system administrator.');
         }
         data = $(data);
         entries = $.parseJSON($('#entryTable td', data).html().replace(/<br>/gi, ''));
         matches = $.parseJSON($('#matchTable td', data).html().replace(/<br>/gi, '')).matches;
         for (entry in entries) {
-            entries[entry].elo = rankMap[entries[entry].rank].elo;
-            entries[entry].wins = 0;
-            entries[entry].losses = 0;
-            entries[entry].username = entry;
+            if (entries.hasOwnProperty(entry) && entries.propertyIsEnumerable(entry)) {
+                entries[entry].username = entry;
+                entries[entry].rankID = rankMap.map(entries[entry].rank).rankID;
+                entries[entry].elo = rankMap.map(entries[entry].rank).elo;
+                entries[entry].wins = 0;
+                entries[entry].losses = 0;
+                // data for statistics below
+                entries[entry].gamesPlayed = 0;
+                entries[entry].higherPlayed = 0;
+                entries[entry].lowerPlayed = 0;
+                entries[entry].equalPlayed = 0;
+                entries[entry].higherWon = 0;
+                entries[entry].lowerWon = 0;
+                entries[entry].equalWon = 0;
+                entries[entry].higherLost = 0;
+                entries[entry].lowerLost = 0;
+                entries[entry].equalLost = 0;
+                entries[entry].winLossHistory = [];
+                // end of statistics
+            }
         }
         matches.forEach(function(v) {
             if (!v.winner || !v.loser) {
@@ -104,6 +211,8 @@ $(function() {
             } else {
                 entries[v.winner].wins++;
                 entries[v.loser].losses++;
+                entries[v.winner].gamesPlayed++;
+                entries[v.loser].gamesPlayed++;
                 calculateNewElo({
                     winner: v.winner,
                     loser: v.loser
@@ -111,15 +220,21 @@ $(function() {
             }
         });
         for (iter in entries) {
-            sortArray.push(entries[iter]);
+            if (entries.hasOwnProperty(iter) && entries.propertyIsEnumerable(iter)) {
+                sortArray.push(entries[iter]);
+            }
         }
-        sortArray = sortArray.sort(function(a, b) {
-            return b.elo - a.elo;
-        });
-        sortArray.forEach(function(v, i) {
-            $('#eloDisplay tbody').append('<tr class="rankingTable rank_' + (i + 1) + '"><td class="rankingTableUser">' + v.username + '</td><td class="rankingTableWins">' + v.wins + '</td><td class="rankingTableLosses">' + v.losses + '</td><td class="rankingTableElo">' + v.elo + '</td></tr>');
-        });
+        $('#eloDisplay tbody').html(makeTable(sortAfter(sortArray, "elo", true)));
         $('#loading').css('display', 'none');
         $('#tableHead').css('display', 'block');
+        /*$('div[class^="ranking"]').on('click', function(e) {
+            if ($(this).hasClass('asc')) {
+                $('#eloDisplay tbody').html(makeTable(sortAfter(sortArray, $(this).removeClass('asc').attr('class').split('ranking')[0], false)));
+                $(this).addClass('desc');
+            } else {
+                $('#eloDisplay tbody').html(makeTable(sortAfter(sortArray, $(this).removeClass('desc').attr('class').split('ranking')[0], true)));
+                $(this).addClass('asc');
+            }
+        });*/
     });
 });
